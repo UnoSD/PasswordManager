02f08fe8642747998a76d150df06136e9e716a4c
UnoSD <unosd.dev@gmail.com>
2017-02-24 21:45:16 +0000

    Refactoring MainWindow and PasswordInputBox.

--- Paccia/MainWindow.xaml.cs
+++ Paccia/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace Paccia
     public partial class MainWindow
     {
         readonly EncryptedRepositoryFactory<Secret> _repositoryFactory;
-        IReadOnlyCollection<Secret> _readOnlyCollection;
+        IList<Secret> _secrets;
         SecureString _passphrase;
 
         public MainWindow(EncryptedRepositoryFactory<Secret> repositoryFactory)
@@ -21,9 +21,32 @@ namespace Paccia
             InitializeComponent();
         }
 
-        Repository<Secret> GetRepository(string passphrase) => _repositoryFactory.GetRepository(passphrase, Environment.MachineName, ConfigurationKey.SecretsFilePath);
+        Repository<Secret> GetRepository(string passphrase) => 
+            _repositoryFactory.GetRepository(passphrase, Environment.MachineName, ConfigurationKey.SecretsFilePath);
 
-        void MainWindowOnLoaded(object sender, EventArgs e) => MasterPasswordInputBox.Show();
+        async void MainWindowOnLoaded(object sender, EventArgs e) =>
+            await DisableUserInteractionsWhile(async () =>
+            {
+                _passphrase = await MasterPasswordInputBox.GetPasswordAsync();
+
+                _secrets = (await GetRepository(_passphrase.ToClearString()).LoadAsync()).ToList();
+
+                EntryListView.ItemsSource = _secrets;
+            });
+
+        Task DisableUserInteractionsWhile(Func<Task> action) =>
+            DisableUserInteractionsWhile(async () => { await action(); return 0; });
+
+        async Task<T> DisableUserInteractionsWhile<T>(Func<Task<T>> action)
+        {
+            MainContent.IsEnabled = false;
+
+            var result = await action();
+
+            MainContent.IsEnabled = true;
+
+            return result;
+        }
 
         void EntryListViewOnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -42,35 +65,32 @@ namespace Paccia
         {
             if (EntrySearchTextBox.Text.Length < 1)
             {
-                EntryListView.ItemsSource = _readOnlyCollection;
+                EntryListView.ItemsSource = _secrets;
                 return;
             }
 
             var lower = EntrySearchTextBox.Text.ToLower();
 
-            var itemsSource = _readOnlyCollection?.Where(secret => secret.Description.ToLower().Contains(lower));
+            var itemsSource = _secrets?.Where(secret => secret.Description.ToLower().Contains(lower));
 
             if (EntryListView != null)
                 EntryListView.ItemsSource = itemsSource;
         }
 
-        async void AddSecretButtonOnClick(object sender, RoutedEventArgs e)
-        {
-            MainContent.IsEnabled = false;
-            
-            var secret = await AddSecretBox.CreateSecretAsync();
-
-            if (secret != null)
+        async void AddSecretButtonOnClick(object sender, RoutedEventArgs e) => 
+            await DisableUserInteractionsWhile(async () =>
             {
-                _readOnlyCollection = _readOnlyCollection.Concat(new[] { secret }).ToArray();
+                var secret = await AddSecretBox.CreateSecretAsync();
 
-                await GetRepository(_passphrase.ToClearString()).SaveAsync(_readOnlyCollection);
+                if (secret == null)
+                    return;
 
-                EntryListView.ItemsSource = _readOnlyCollection;
-            }
+                _secrets.Add(secret);
 
-            MainContent.IsEnabled = true;
-        }
+                await GetRepository(_passphrase.ToClearString()).SaveAsync(_secrets);
+
+                EntryListView.Items.Refresh();
+            });
 
         void ShowSecretButtonOnClick(object sender, RoutedEventArgs e) => SecretTextBox.Visibility = Visibility.Visible;
 
@@ -112,25 +132,5 @@ namespace Paccia
 
             SecretTextBox.Text = selectedItem?.Value;
         }
-        
-        async Task LoadAndPopulateList(SecureString passphrase)
-        {
-            // Use keylogger prevention.
-            // Maybe: 1-3-5 character of the passphrase first
-            // then 2-4-6...
-            _passphrase = passphrase;
-
-            IsEnabled = false;
-
-            _readOnlyCollection = await GetRepository(_passphrase.ToClearString()).LoadAsync();
-
-            EntryListView.ItemsSource = _readOnlyCollection;
-
-            IsEnabled = true;
-        }
-        
-        async void MasterPasswordInputBoxPasswordInserted(object sender, SecureString e) => await LoadAndPopulateList(e);
-
-        void MasterPasswordInputBoxOnCancel(object sender, EventArgs e) => Close();
     }
 }
