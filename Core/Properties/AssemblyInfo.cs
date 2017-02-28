0cb8b97a98bfe9554c992bff12202654044954ab
UnoSD <unosd.dev@gmail.com>
2017-02-28 22:11:21 +0000

    Removing unused packages and references from Paccia.

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Core")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Stefano d'Antonio 2017")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
