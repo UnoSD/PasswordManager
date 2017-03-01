882a43fe86fe03f9ac8ccf1ee9ec95a0429dfbca
UnoSD <unosd.dev@gmail.com>
2017-03-01 21:29:05 +0000

    Simplified test controller and added url to Secret.

--- Core/Secret.cs
+++ Core/Secret.cs
@@ -4,6 +4,7 @@ namespace Paccia
 {
     public class Secret
     {
+        public string Url { get; set; }
         public string Description { get; set; }
         public IDictionary<string, string> Fields { get; } = new Dictionary<string, string>();
         public IDictionary<string, string> Secrets { get; } = new Dictionary<string, string>();
--- WebServer/Controllers/SecretsController.cs
+++ WebServer/Controllers/SecretsController.cs
@@ -3,54 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Paccia;
 
 namespace WebServer.Controllers
 {
+    [Authorize]
     [Route("[controller]")]
-    public class SecretsController : Controller
+    public partial class SecretsController : Controller
     {
         [HttpGet("{url}")]
-        public async Task<object> GetAsync(string url)
+        public async Task<IEnumerable<Secret>> GetAsync(string url)
         {
             var sourceUri = new Uri(WebUtility.UrlDecode(url));
 
-            return await GetSecretByUri(sourceUri.Host);
+            var secrets = await CreateTestRepository().LoadAsync();
+
+            return secrets.Where(secret => secret.Description == sourceUri.Host);
         }
 
-        static async Task<IEnumerable<Secret>> GetSecretByUri(string host)
+        [HttpPost]
+        public async Task PostAsync(Secret secret)
         {
-            var aesEncryptorDecryptor = new AesEncryptorDecryptor();
-            var shaHeaderGenerator = new ShaHeaderGenerator();
-            var dataSerializer = new JsonSerializer<IEnumerable<Secret>>();
-            var shaAesEncryptor = new ShaAesEncryptor(aesEncryptorDecryptor, shaHeaderGenerator);
-            var shaAesDecryptor = new ShaAesDecryptor(aesEncryptorDecryptor, shaHeaderGenerator);
-            var hardcodedConfigurationDefaults = new HardcodedConfigurationDefaults();
-            var configuration = new Configuration(hardcodedConfigurationDefaults);
-            var logger = new Logger();
+            var repository = CreateTestRepository();
 
-            var encryptionSerializersFactory = new EncryptionSerializersFactory<IEnumerable<Secret>>(dataSerializer, shaAesEncryptor, shaAesDecryptor);
-
-            var encryptionSerializer = encryptionSerializersFactory.GetSerializer("pippo".ToSecureString(), "pepper");
-
-            var userFileStorageProvider = new UserFileStorageProvider(configuration, ConfigurationKey.SecretsFilePath, logger);
-
-            var repository = new Repository<Secret>(encryptionSerializer, userFileStorageProvider);
-
-            await AddSamples(host, repository);
-
-            return (await repository.LoadAsync()).Where(secret => secret.Description == host);
-        }
-
-        static async Task AddSamples(string host, Repository<Secret> repository)
-        {
-            var secrets = new[]
-                          {
-                              new Secret {Description = host},
-                              new Secret {Description = "http://www.google.it"}
-                          };
-            await repository.SaveAsync(secrets);
+            var secrets = await repository.LoadAsync();
+            
+            await repository.SaveAsync(secrets.Concat(new [] { secret }));
         }
     }
 }
