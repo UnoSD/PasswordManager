43288acfd3f4ec0daca73ae4e7819aa9824ad365
UnoSD <unosd.dev@gmail.com>
2017-03-04 22:32:01 +0000

    Keyboard bindings for web extension.

--- WebExtension/app.ts
+++ WebExtension/app.ts
@@ -1,42 +1,79 @@
-console.log("Loading Paccia web extension.");
+var trap = getGlobalKeyboardShortcuts();
 
-const protocol = "http";
-const host = "localhost";
-const port = 5000;
-const path = "/secrets/http%3A%2F%2Fwww.g";
-const appId = "92C9C40B-BB2F-4813-8FA8-39628AD7EA4E";
-const secretKey = "CB6C5713-E213-4524-B93C-17020F0B5B4B";
-const httpMethod = "GET";
-const nonce = Math.random().toString(36).substring(2, 15);
-const content = "";
-const time = Date.now() / 1000 | 0;
+trap.bind("ctrl+shift+u", (e) => setTextValueFrom(getUserName, e.srcElement));
 
-const dataToHash = `${appId}${httpMethod}${path}${time}${nonce}${content}`;
+trap.bind("ctrl+shift+p", (e) => setTextValueFrom(getPassword, e.srcElement));
 
-const signature = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(dataToHash, secretKey));
+function getGlobalKeyboardShortcuts() {
+    const trap = new Mousetrap(window.frameElement);
 
-console.log("Sending the request.");
+    trap.stopCallback = () => false;
 
-$.ajaxSetup({
-    headers: { "CustomHeader": "myValue" }
-});
+    return trap;
+}
 
-$.ajax({
-    type: httpMethod,
-    url: `${protocol}://${host}:${port}${path}`,
-    crossDomain: true,
-    beforeSend(request) {
-        request.setRequestHeader("Authorization", `Hmac ${appId}:${signature}:${nonce}:${time}`);
-    },
-    headers: {
-        "Authorization": `Hmac ${appId}:${signature}:${nonce}:${time}`
-    },
-    success(msg) {
-        console.log(msg);
-    },
-    error(request, status, error) {
-        console.log(error);
+function setTextValueFrom(getValue: () => string, element: any) {
+    const activeElement: any = document.activeElement;
+
+    if (!($(activeElement).is("input") || $(activeElement).is("textarea"))) {
+        alert("You must be in the password box to insert the password.");
+        return;
     }
-});
 
-console.log("Request sent.");
+    const userName = getValue();
+
+    activeElement.value = userName;
+}
+
+function getPassword() {
+    return "password";
+};
+
+function getUserName() {
+    return "username";
+};
+
+function callServer () {
+    const protocol = "http";
+    const host = "localhost";
+    const port = 5000;
+    const path = "/secrets/http%3A%2F%2Fwww.g";
+    const appId = "92C9C40B-BB2F-4813-8FA8-39628AD7EA4E";
+    const secretKey = "CB6C5713-E213-4524-B93C-17020F0B5B4B";
+    const httpMethod = "GET";
+    const nonce = Math.random().toString(36).substring(2, 15);
+    const content = "";
+    const time = Date.now() / 1000 | 0;
+
+    const dataToHash = `${appId}${httpMethod}${path}${time}${nonce}${content}`;
+
+    const signature = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(dataToHash, secretKey));
+
+    console.log("Sending the request.");
+
+    $.ajaxSetup({
+        headers: { "CustomHeader": "myValue" }
+    });
+
+    $.ajax({
+        type: httpMethod,
+        url: `${protocol}://${host}:${port}${path}`,
+        crossDomain: true,
+        beforeSend(request) {
+            request.setRequestHeader("Authorization", `Hmac ${appId}:${signature}:${nonce}:${time}`);
+        },
+        headers: {
+            "Authorization": `Hmac ${appId}:${signature}:${nonce}:${time}`
+        },
+        success(msg) {
+            console.log(msg);
+        },
+        error(request, status, error) {
+            console.log(error);
+        }
+    });
+
+    console.log("Request sent.");
+
+    return "";
+}
